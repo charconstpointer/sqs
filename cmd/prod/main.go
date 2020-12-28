3b329b7703c81dc025bdae1577842d796e4a5770
charconstpointer <attwistingnether@gmail.com>
2020-12-28 01:25:41 +0100

    init

---
+++ cmd/prod/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+func main() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := sqs.New(sess)
+	queueURL := "https://sqs.eu-west-2.amazonaws.com/886197426502/kitty"
+	_, err := svc.SendMessage(&sqs.SendMessageInput{
+		DelaySeconds: aws.Int64(10),
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"Title": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String("The Whistler"),
+			},
+			"Author": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String("John Grisham"),
+			},
+			"WeeksOn": {
+				DataType:    aws.String("Number"),
+				StringValue: aws.String("6"),
+			},
+		},
+		MessageBody: aws.String("Information about current NY Times fiction bestseller for week of 12/11/2016."),
+		QueueUrl:    &queueURL,
+	})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
---
+++ cmd/recv/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+func main() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := sqs.New(sess)
+	queueURL := "https://sqs.eu-west-2.amazonaws.com/886197426502/kitty"
+	msgResult, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+		AttributeNames: []*string{
+			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
+		},
+		MessageAttributeNames: []*string{
+			aws.String(sqs.QueueAttributeNameAll),
+		},
+		QueueUrl:            &queueURL,
+		MaxNumberOfMessages: aws.Int64(1),
+		VisibilityTimeout:   aws.Int64(123),
+	})
+	log.Println(*msgResult.Messages[0].Body)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
