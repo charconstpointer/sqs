4fc09e623fd55d8a5e10f1612bee250f2b6caee6
charconstpointer <attwistingnether@gmail.com>
2020-12-28 01:48:05 +0100

    interactive loops

--- cmd/prod/main.go
+++ cmd/prod/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -26,28 +28,19 @@ func main() {
 	}))
 
 	svc := sqs.New(sess)
+	s := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("msg >")
+		s.Scan()
+		body := s.Text()
+		_, err := svc.SendMessage(&sqs.SendMessageInput{
+			DelaySeconds: aws.Int64(0),
+			MessageBody:  aws.String(body),
+			QueueUrl:     queueURL,
+		})
 
-	_, err := svc.SendMessage(&sqs.SendMessageInput{
-		DelaySeconds: aws.Int64(10),
-		MessageAttributes: map[string]*sqs.MessageAttributeValue{
-			"Title": {
-				DataType:    aws.String("String"),
-				StringValue: aws.String("The Whistler"),
-			},
-			"Author": {
-				DataType:    aws.String("String"),
-				StringValue: aws.String("John Grisham"),
-			},
-			"WeeksOn": {
-				DataType:    aws.String("Number"),
-				StringValue: aws.String("6"),
-			},
-		},
-		MessageBody: aws.String("Information about current NY Times fiction bestseller for week of 12/11/2016."),
-		QueueUrl:    queueURL,
-	})
-
-	if err != nil {
-		log.Fatal(err.Error())
+		if err != nil {
+			log.Fatal(err.Error())
+		}
 	}
 }
--- cmd/recv/main.go
+++ cmd/recv/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -26,21 +28,31 @@ func main() {
 	}))
 
 	svc := sqs.New(sess)
-
-	msgResult, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
-		AttributeNames: []*string{
-			aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
-		},
-		MessageAttributeNames: []*string{
-			aws.String(sqs.QueueAttributeNameAll),
-		},
-		QueueUrl:            queueURL,
-		MaxNumberOfMessages: aws.Int64(1),
-		VisibilityTimeout:   aws.Int64(123),
-	})
-	log.Println(*msgResult.Messages[0].Body)
-
-	if err != nil {
-		log.Fatal(err.Error())
+	s := bufio.NewScanner(os.Stdin)
+	for {
+		log.Println("type anything to fetch messages")
+		s.Scan()
+		msgResult, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+			AttributeNames: []*string{
+				aws.String(sqs.MessageSystemAttributeNameSentTimestamp),
+			},
+			MessageAttributeNames: []*string{
+				aws.String(sqs.QueueAttributeNameAll),
+			},
+			QueueUrl:            queueURL,
+			MaxNumberOfMessages: aws.Int64(1),
+			VisibilityTimeout:   aws.Int64(123),
+		})
+		msgCount := len(msgResult.Messages)
+		if msgCount == 0 {
+			log.Println("no messages to fetch")
+			continue
+		}
+		for i, msg := range msgResult.Messages {
+			log.Printf("%d %s", i, *msg.Body)
+		}
+		if err != nil {
+			log.Fatal(err.Error())
+		}
 	}
 }
